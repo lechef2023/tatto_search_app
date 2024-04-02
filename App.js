8bbe3bf72f8a59d56b3a8229f5307b3658d3e4cc
motoredes <motoredes2021@gmail.com>
2024-04-02 17:18:19 -0400

    Funciona la Nav beetwen screams

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ImageBackground, Button } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <ImageBackground
+      source={require('./images/fondo.jpg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello Tatto World!</Text>
+        <TextInput style={styles.textinput} defaultValue="Realiza tu búsqueda" />
+        <Button onPress={() => navigation.navigate('Notifications')} title="Go to notifications" />
+      </View>
+    </ImageBackground>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <ImageBackground
+      source={require('./images/fondo.jpg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={() => navigation.goBack()} title="Go back home" />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerStyle={{ backgroundColor: 'black' }}
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textinput: {
+    height: 40,
+    borderColor: 'white',
+    borderWidth: 2,
+    color: 'white',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
   },
 });
